779d753e83cfd54c77501fc3546641dacc0dd765
Rishabh Gupta <rishabhindia2006@gmaili.com>
2025-08-01 00:58:56 +0530

    love

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Birthday Surprise',
-  description: 'Created with v0',
+  description: 'Created with ❤️ for My Love',
 }
 
 export default function RootLayout({
